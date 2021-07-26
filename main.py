01a1c9619cfadb1e20966253ebabd0987659619a
MOSES O.A <zizmusic99@gmail.com>
2021-07-26 23:14:21 +0100

    Added gitignore

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
 
-
-MY_EMAIL = 'femismpt@gmail.com'
-MY_PASSWORD = 'femismpt123..'
+MY_EMAIL = os.environ.get("EMAIL")
+MY_PASSWORD = os.environ.get("PASSWORD")
 
 
 app = Flask(__name__)
